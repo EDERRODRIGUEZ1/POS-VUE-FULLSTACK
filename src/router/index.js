20a3d17a040b65fa27e05938fdda508daffd07eb
EDERRODRIGUEZ1 <68400379+EDERRODRIGUEZ1@users.noreply.github.com>
2023-10-15 22:53:10 -0600

    Code 314. Routing y titulos a las paginas de administracion

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ShopView from '../views/ShopView.vue'
+import AdminLayout from '../views/admin/AdminLayout.vue'
 
 
 const router = createRouter({
@@ -7,9 +8,33 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'shop',
       component: ShopView
     },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminLayout,
+      children: [
+        {
+          path: 'productos',
+          name: 'products',
+          component: () => import('../views/admin/ProductsView.vue')
+        },
+        {
+          path: 'productos/nuevo',
+          name: 'new-product',
+          component: () => import('../views/admin/NewProductView.vue')
+        },
+        {
+          path: 'ventas',
+          name: 'sales',
+          component: () => import('../views/admin/SalesView.vue')
+        }
+
+      ]
+    }
+
   ]
 })
 
---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import Link from '../../components/Link.vue';
+</script>
+
+<template>
+  <div class="">
+    <Link
+      to="new-product"
+    >
+      Nuevo Producto
+    </Link>
+    <h1 class="text-4xl font-black my-10">Productos</h1>
+  </div>
+</template>
