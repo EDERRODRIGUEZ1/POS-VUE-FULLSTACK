1fab55b068ceec5fa692c299298d1a42d8158a72
EDERRODRIGUEZ1 <68400379+EDERRODRIGUEZ1@users.noreply.github.com>
2023-10-22 23:03:39 -0600

    code hasta 355 Eliminar elementos del carrito

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,43 @@
+<script setup>
+    import ShoppingCartItem from './ShoppingCartItem.vue';
+    import Amount from './Amount.vue';
+    import { useCartStore } from '../stores/cart';
+import { formatCurrency } from '../helpers';
+
+    const cart = useCartStore()
+</script>
+
+<template>
+ 
+    <p v-if="cart.isEmpty" class="text-xl text-gray-900">El Carrito esta vacio</p>
+    <div v-else>
+        <p class="text-4xl font-bold text-gray-900"> Resumen de Venta</p>
+        <ul
+            role="list"
+            class="mt-6 divide-y divide-gray-200 border-gray-200"
+        >
+            <ShoppingCartItem 
+                v-for="item in cart.items"
+                :key="item.id"
+                :item="item"
+            />
+        </ul>
+
+        <dl class="space-y-6 border-t border-green-200 pt-6 text-sm font-medium text-gray-500">
+            <Amount>
+                <template #label>Subtotal:</template>
+                {{ formatCurrency(cart.subtotal) }}
+            </Amount>
+
+            <Amount>
+                <template #label>Impuesto:</template>
+                {{ formatCurrency(cart.taxes) }}
+            </Amount>
+
+            <Amount>
+                <template #label>Total a pagar:</template>
+                {{ formatCurrency(cart.total) }}
+            </Amount>
+        </dl>
+    </div>
+</template>
---
+++ src/stores/cart.js
@@ -0,0 +1,81 @@
+import { ref, computed, watch, watchEffect } from 'vue'
+import { defineStore } from 'pinia';
+
+export const useCartStore = defineStore('cart', () => {
+
+    const items = ref([])
+    const subtotal = ref(0)
+    const taxes = ref(0) //state de impuesto
+    const total = ref(0)
+
+    const MAX_PRODUCTS = 5
+    const TAX_RATE = .10  //IMPUESTO
+
+    // watch(items, () => {
+    //     //el cero es le valor inicial
+    //     subtotal.value = items.value.reduce((total , item) => total + (item.quantity * item.price), 0)
+    //     //calcular el impuesto
+    //     taxes.value = subtotal.value * TAX_RATE
+    //     //total
+    //     total.value = subtotal.value + taxes.value
+    // }, {
+    //     deep: true
+    // })
+
+    watchEffect(() =>{
+        //el cero es le valor inicial
+        subtotal.value = items.value.reduce((total , item) => total + (item.quantity * item.price), 0)
+        //calcular el impuesto
+        taxes.value = subtotal.value * TAX_RATE
+        //total
+        total.value = subtotal.value + taxes.value
+    })
+
+    function addItem(item) {
+        const index = isItemInCart(item.id)
+        if(index >= 0) {
+            if(isProductAvailable( item, index)) {
+                alert('Has alcanzado el limite')
+                return
+            }
+            // Si existe se DEBE ACTUALIZAR LA CANTIDAD
+            items.value[index].quantity++
+        }else{
+            items.value.push({...item, quantity: 1, id: item.id})
+        }
+    }
+
+    function updateQuantity(id, quantity) {
+        items.value = items.value.map( item => item.id === id ? {...item, quantity} : item)
+    }
+
+    function removeItem(id) {
+        items.value = items.value.filter( item => item.id !== id)
+    }
+
+    //se encarga de comprobar si el elemento ya existe en el carrito
+    const isItemInCart = id => items.value.findIndex( item => item.id === id) //findIndex regresa la posicion del elemento del arreglo
+    
+    //se encarga de comprobar si hay disponibles
+    const isProductAvailable = ( item, index) =>{
+        return items.value[index].quantity >= item.availability || items.value[index].quantity >= MAX_PRODUCTS
+    }
+
+    const isEmpty = computed(() => items.value.length === 0);
+
+    const checkProductAvailability = computed(() => {
+        return (product) => product.availability < MAX_PRODUCTS ? product.availability : MAX_PRODUCTS
+    })
+    return {
+        items,
+        subtotal,
+        taxes,
+        total,
+        addItem,
+        updateQuantity,
+        removeItem,
+        isEmpty,
+        checkProductAvailability,
+       
+    }
+});
